a0c477fe909094ea909d4e4e9f00b2904062b659
kvsiva <sivakrishna.konijeti@gmail.com>
2020-02-06 17:06:37 +0530

    Test Commit 2

--- src/main/java/com/sv/restapi/lab1/AddressBookController.java
+++ src/main/java/com/sv/restapi/lab1/AddressBookController.java
@@ -1,4 +1,4 @@
-package com.sv.controller;
+package com.sv.restapi.lab1;
 
 import com.sv.entity.Contact;
 import org.springframework.web.bind.annotation.*;
@@ -9,6 +9,7 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
 @RestController
+@RequestMapping(path = "/ab")
 public class AddressBookController {
     ConcurrentMap<String, Contact> contacts=new ConcurrentHashMap<>();
 
@@ -19,11 +20,12 @@ public class AddressBookController {
 
      @GetMapping("/")
      public List<Contact> getAllContacts(){
-         return new ArrayList<Contact>(contacts.values());
+         return new ArrayList<>(contacts.values());
      }
 
-    @GetMapping("/hello")
-    public String getStatus(){
+     @GetMapping(path="/greeting", produces = "text/html")
+
+    public String greeting(){
         return "Hi Welcome to Java REST API";
     }
 
