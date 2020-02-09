b61f42f485e0bbae0b37e15bdcd2b783bd465d5f
kvsiva <sivakrishna.konijeti@gmail.com>
2020-02-09 07:16:19 +0530

    Test Commit 5 -Added Constructor for entity class

--- src/main/java/com/sv/restapi/lab1/entity/Contact.java
+++ src/main/java/com/sv/restapi/lab1/entity/Contact.java
@@ -2,6 +2,17 @@ package com.sv.restapi.lab1.entity;
 
 public class Contact {
     private String id;
+
+    public Contact() {
+    }
+
+    public Contact(String id, String name, String address, int mobileNumber) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.mobileNumber = mobileNumber;
+    }
+
     private String name;
     private String address;
     private int mobileNumber;
