58ae3473e76ab8a8304cc27f248a676562b9ca9f
kvsiva <sivakrishna.konijeti@gmail.com>
2020-08-20 19:29:01 +0530

    Created DOCKET and Adding Application metadata

--- src/main/java/com/sv/restapi/lab1/Application.java
+++ src/main/java/com/sv/restapi/lab1/Application.java
@@ -2,7 +2,16 @@ package com.sv.restapi.lab1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collection;
+import java.util.Collections;
 
 @SpringBootApplication
 @EnableSwagger2
@@ -10,6 +19,27 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+    @Bean
+	public Docket swaggerConfiguration(){
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.paths(PathSelectors.ant("/ab/*"))
+				.apis(RequestHandlerSelectors.basePackage("com.sv"))
+				.build()
+				.apiInfo(apiDetails());
+	}
+	private ApiInfo apiDetails(){
+		return new ApiInfo(
+				"REST API Samples",
+				"Sample API",
+				"1.0",
+				"Free to use",
+				new springfox.documentation.service.Contact("siva","https://mongodblog.wordpress.com/","s@v@gmail.com"),
+				"API Licence",
+				"https://mongodblog.wordpress.com/",
+				Collections.emptyList());
+
 	}
 
 }
